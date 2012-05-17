5d2385df365bce23e2a579130f1af9b25dc9123e
Chris Price <price.c@gmail.com>
2012-05-17 15:45:41 +0100

    fix goal annotation

--- plugin/src/main/java/com/github/chrisprice/phonegapbuild/plugin/BuildMojo.java
+++ plugin/src/main/java/com/github/chrisprice/phonegapbuild/plugin/BuildMojo.java
@@ -4,13 +4,12 @@ import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
 
+/**
+ * Says "Hi" to the user.
+ * 
+ * @goal sayhi
+ */
 public class BuildMojo extends AbstractMojo {
-
-  /**
-   * Says "Hi" to the user.
-   * 
-   * @goal sayhi
-   */
   public void execute() throws MojoExecutionException, MojoFailureException {
     getLog().info("Hello, world.");
   }
